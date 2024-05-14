b989b7094c171e540dfd2fa024d0dedd42249a5a
Miguel <miguld24@gmail.com>
2024-05-14 15:10:44 -0500

    add github actions

--- api/src/config/index.js
+++ api/src/config/index.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 const config = {
   env: process.env.NODE_ENV || 'dev',
   isProd: process.env.NODE_ENV === 'production',
-  port: process.env.PORT || 3000,
+  port: process.env.PORT || 3002,
   dbUrl: process.env.MONGO_URL,
   dbName: process.env.MONGO_DB_NAME,
 };
